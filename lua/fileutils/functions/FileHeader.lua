353cf7cca70c1e2566d3a9f7436f1ce59de03fed
Indy <Indy@debian.org>
2025-06-11 10:52:45 -0400

    added some more lang supports for the lang

--- lua/fileutils/functions/FileHeader.lua
+++ lua/fileutils/functions/FileHeader.lua
@@ -6,12 +6,7 @@ function M.InsertFileHeader()
   -- Get system username, date/time, and file type
   local user = os.getenv "USER" or os.getenv "USERNAME" or "unknown"
   local date_time = os.date "%A %B %d, %Y, %I:%M %p"
-  local file_type = vim.bo.filetype
-
-  -- Replaces lang with unknown if not known
-  if not file_type or file_type == "" then
-    file_type = "unknown"
-  end
+  local file_type = vim.bo.filetype or "unknown"
 
   -- Define a table mapping file types to comment styles
   local comment_styles = {
@@ -25,38 +20,69 @@ function M.InsertFileHeader()
     jsonc = "#",
     yaml = "#",
     toml = "#",
+    make = "#",
+    dockerfile = "#",
+    ini = "#",
+    perl = "#",
 
     -- "//" Comments
     javascript = "//",
+    typescript = "//",
     c = "//",
     cpp = "//",
+    rust = "//",
+    java = "//",
+    kotlin = "//",
+    r = "//",
+    swift = "//",
+    scala = "//",
+    groovy = "//",
+    glsl = "//",
 
     -- "Wrap Around" Comments
     html = { "<!--", "-->" },
-    css = { "/*", "*/" },
+    xml = { "<!--", "-->" },
     markdown = { "<!--", "-->" },
+    css = { "/*", "*/" },
+    scss = { "/*", "*/" },
+    less = { "/*", "*/" },
 
-    -- "Other" Comments
+    -- "Other" styles
     lua = "--",
     vim = '"',
     autohotkey = ";",
+    assembly = ";",
+    lisp = ";",
+    scheme = ";",
+    clojure = ";",
+    elisp = ";",
+    tcl = "#",
+    sql = "--",
+    haskell = "--",
+    ada = "--",
+    rebol = ";",
+    fortran = "!",
+    erlang = "%",
+    prolog = "%",
+    fsharp = "//",
+    ocaml = { "(*", "*)" },
+    coq = { "(*", "*)" },
+    sml = { "(*", "*)" },
   }
 
-  -- Determine the comment style for the current file type
+  -- Fallback to "#" if unknown
   local comment_symbol = comment_styles[file_type] or "#"
 
-  -- Construct the comment string
-  local comment
+  -- Construct the comment header
+  local header
   if type(comment_symbol) == "table" then
-    -- If comment_symbol is a table, use it as opening and closing comment
-    comment = string.format("%s By: %s | %s | %s %s", comment_symbol[1], user, date_time, file_type, comment_symbol[2])
+    header = string.format("%s By: %s | %s | %s %s", comment_symbol[1], user, date_time, file_type, comment_symbol[2])
   else
-    -- For single-line comments
-    comment = string.format("%s By: %s | %s | %s", comment_symbol, user, date_time, file_type)
+    header = string.format("%s By: %s | %s | %s", comment_symbol, user, date_time, file_type)
   end
 
-  -- Insert the comment at the top of the file
-  vim.api.nvim_buf_set_lines(0, 0, 0, false, { comment })
+  -- Insert header at the top of the buffer
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, { header })
 end
 
 return M
